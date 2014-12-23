fdde55ef2a53a7a26a2cf688e4977a4b8ef105ed
Abhinay Thakur <a8h1nay@gmail.com>
2014-12-23 20:01:11 +0530

    Added functionality of using decimal numbers
    Now decimal numbers can be entered with accuracy and correctly :wink:

--- prototype/CalculatorDemo.java
+++ prototype/CalculatorDemo.java
@@ -11,6 +11,7 @@ public class CalculatorDemo{
 	private Button b3;
 	private Button b4;
 	private Button b5;
+	private Button b6;
 	private TextField inBox;
 	private Panel p1;
 	private Panel p2;
@@ -25,6 +26,7 @@ public class CalculatorDemo{
 		b3 = new Button("2");
 		b4 = new Button("=");
 		b5 = new Button("-");
+		b6 = new Button(".");
 
 		inBox = new TextField(50);
 
@@ -44,12 +46,14 @@ public class CalculatorDemo{
 		p2.add(b3);
 		p2.add(b4);
 		p2.add(b5);
+		p2.add(b6);
 
 		b1.addActionListener(h);
 		b2.addActionListener(h);
 		b3.addActionListener(h);
 		b4.addActionListener(h);
 		b5.addActionListener(h);
+		b6.addActionListener(h);
 
 		f.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent we){
@@ -71,7 +75,67 @@ public class CalculatorDemo{
 
 		private Double valueOne;
 		private Double valueTwo;
-		private Double answer;
+		private Double answer;	
+
+		private int decimalCount = 0;
+
+		public void actionPerformed(ActionEvent e){
+			if(e.getSource() == b1)
+				displayFormatting("1");
+
+			if(e.getSource() == b2)
+				displayFormatting("+");
+
+			if(e.getSource() == b3)
+				displayFormatting("2");
+
+			if(e.getSource() == b4)
+				displayFormatting("=");
+
+			if(e.getSource() == b5)
+				displayFormatting("-");
+
+			if(e.getSource() == b6){
+				if (decimalCount == 0){
+					displayFormatting(".");
+					decimalCount = 1;
+				}
+			}
+		}
+
+		public void displayFormatting(String num){
+			
+			initString = inBox.getText();
+			finalString = initString;
+			
+
+			System.out.println("Occurence 1 | initString: " + initString);
+			System.out.println("Occurence 1 | finalString: " + finalString);
+
+			if(num == "+" || num == "-"){
+				sign = num;
+				inBox.setText("");
+				decimalCount = 0;
+				valueOne = Double.parseDouble(finalString);
+				System.out.println("Occurence A | valueOne: " + valueOne);
+			}
+			else if(num == "="){
+				inBox.setText("");
+				decimalCount = 0;
+				System.out.println("Decimal Count: " + decimalCount + " || Occurence 1");
+				calculateResult(sign);
+			}
+			else{
+				currentString = num;
+				finalString = initString + currentString;
+				inBox.setText(finalString);
+					
+				System.out.println("Occurence 2 | finalString: " + finalString);
+			}				
+
+			System.out.println("Occurence 3 | finalString: " + finalString);
+			System.out.println(" ");			
+		}
 
 		public void calculateResult(String sn){
 			switch(sn){
@@ -90,52 +154,6 @@ public class CalculatorDemo{
 			}
 			System.out.println("Occurence A | valueTwo: " + valueTwo);
 			System.out.println("Occurence A | answer: " + answer);
-		}
-
-		public void displayFormatting(String num){
-			initString = inBox.getText();
-			finalString = initString;
-			
-
-			System.out.println("Occurence 1 | initString: " + initString);
-			System.out.println("Occurence 1 | finalString: " + finalString);
-
-			if(num == "+" || num == "-"){
-				sign = num;
-				inBox.setText("");
-				valueOne = Double.parseDouble(finalString);
-				System.out.println("Occurence A | valueOne: " + valueOne);
-			}
-			else if(num == "="){
-				inBox.setText("");
-				calculateResult(sign);
-			}
-			else{
-				currentString = num;
-				finalString = initString + currentString;
-				inBox.setText(finalString);
-				System.out.println("Occurence 2 | finalString: " + finalString);
-			}				
-
-			System.out.println("Occurence 3 | finalString: " + finalString);
-			System.out.println(" ");			
-		}
-
-		public void actionPerformed(ActionEvent e){
-			if(e.getSource() == b1)
-				displayFormatting("1");
-
-			if(e.getSource() == b2)
-				displayFormatting("+");
-
-			if(e.getSource() == b3)
-				displayFormatting("2");
-
-			if(e.getSource() == b4)
-				displayFormatting("=");
-
-			if(e.getSource() == b5)
-				displayFormatting("-");
 		}
 	}
 
